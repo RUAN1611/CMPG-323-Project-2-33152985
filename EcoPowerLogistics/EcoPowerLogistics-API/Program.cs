4e0aa56ccacd0a79e547dabe20d30218e132c2c5
RUAN1611 <fregtonmasdor@gmail.com>
2023-08-18 17:33:08 +0300

    - Added Startup.cs file - Moved Dependency Injection to Startup.cs

--- EcoPowerLogistics/EcoPowerLogistics-API/Program.cs
+++ EcoPowerLogistics/EcoPowerLogistics-API/Program.cs
@@ -1,7 +1,9 @@
+using EcoPowerLogistics_API;
 using EcoPowerLogistics_API.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration); // Added Startup.cs for adding custom dependencies
 
 // Add services to the container.
 
@@ -9,18 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ecopowerlogisticsdevContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-));
+
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
---
+++ EcoPowerLogistics/EcoPowerLogistics-API/Startup.cs
@@ -0,0 +1,22 @@
+﻿using EcoPowerLogistics_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoPowerLogistics_API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ecopowerlogisticsdevContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")
+            ));
+        }
+    }
+}
