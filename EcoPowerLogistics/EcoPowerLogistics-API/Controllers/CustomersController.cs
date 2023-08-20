ccefa6b87d322872d64d8ad53c84efb0500fc536
RUAN1611 <fregtonmasdor@gmail.com>
2023-08-20 14:32:22 +0300

    - Added Documentation Screenshots - Added Response Types for Status Codes in Customer Controller - Added JsonPatch NuGet Package - Added Patch Method to Customer Controller - Changed Return Type for Successful Put

--- EcoPowerLogistics/EcoPowerLogistics-API/Controllers/CustomersController.cs
+++ EcoPowerLogistics/EcoPowerLogistics-API/Controllers/CustomersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using EcoPowerLogistics_API.Models;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace EcoPowerLogistics_API.Controllers
 {
@@ -22,17 +23,21 @@ namespace EcoPowerLogistics_API.Controllers
 
         // GET: api/Customers
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
         {
           if (_context.Customers == null)
           {
               return NotFound();
           }
-            return await _context.Customers.ToListAsync();
+            return Ok(await _context.Customers.ToListAsync());
         }
 
         // GET: api/Customers/5
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Customer>> GetCustomer(short id)
         {
           if (_context.Customers == null)
@@ -46,12 +51,16 @@ namespace EcoPowerLogistics_API.Controllers
                 return NotFound();
             }
 
-            return customer;
+            return Ok(customer);
         }
 
         // PUT: api/Customers/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> PutCustomer(short id, Customer customer)
         {
             if (id != customer.CustomerId)
@@ -77,12 +86,13 @@ namespace EcoPowerLogistics_API.Controllers
                 }
             }
 
-            return NoContent();
+            return Ok(customer);
         }
 
         // POST: api/Customers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
         {
           if (_context.Customers == null)
@@ -109,8 +119,34 @@ namespace EcoPowerLogistics_API.Controllers
             return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
         }
 
+        [HttpPatch("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> PatchCustomer(int id, [FromBody] JsonPatchDocument<Customer> patchCustomer)
+        {
+            if(id == 0 || patchCustomer == null)
+            {
+                return BadRequest();
+            }
+            var customer = _context.Customers.FirstOrDefault(x => x.CustomerId == id);
+            if(customer == null)
+            {
+                return BadRequest();
+            }
+            patchCustomer.ApplyTo(customer, ModelState);
+            await _context.SaveChangesAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(customer);
+        }
+
         // DELETE: api/Customers/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteCustomer(short id)
         {
             if (_context.Customers == null)
--- EcoPowerLogistics/EcoPowerLogistics-API/Program.cs
+++ EcoPowerLogistics/EcoPowerLogistics-API/Program.cs
@@ -7,7 +7,7 @@ var startup = new Startup(builder.Configuration); // Added Startup.cs for adding
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
